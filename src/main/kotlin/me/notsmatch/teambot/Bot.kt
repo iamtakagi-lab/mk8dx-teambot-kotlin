6f1b2e60c317453e76aff9f20cb4627995ea5c88
notsmatch <smatchjp@gmail.com>
2020-07-02 15:23:40 +0900

    fix: public url

--- src/main/kotlin/me/notsmatch/teambot/Bot.kt
+++ src/main/kotlin/me/notsmatch/teambot/Bot.kt
@@ -1,16 +1,17 @@
 package me.notsmatch.teambot
 
 import com.jagrosh.jdautilities.command.CommandClientBuilder
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter
 import me.notsmatch.teambot.command.ChooseCommand
+import me.notsmatch.teambot.command.GuildlistCommand
 import me.notsmatch.teambot.command.TagCommand
 import me.notsmatch.teambot.command.TeamCommand
-import net.dv8tion.jda.api.AccountType
-import net.dv8tion.jda.api.JDA
-import net.dv8tion.jda.api.JDABuilder
-import net.dv8tion.jda.api.OnlineStatus
+import me.notsmatch.tracktablebot.command.AboutCommand
+import net.dv8tion.jda.api.*
 import net.dv8tion.jda.api.entities.Activity
 import net.dv8tion.jda.api.events.ReadyEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.awt.Color
 import java.util.*
 
 
@@ -25,13 +26,21 @@ class Bot (private val token: String) {
     }
 
     lateinit var jda: JDA
+    val eventWaiter = EventWaiter()
+    val WEBSITE = System.getenv("WEBSITE")
 
     fun start() {
         instance = this
         random = Random()
         jda = JDABuilder(AccountType.BOT).setToken(token).setStatus(OnlineStatus.ONLINE).build()
         val builder = CommandClientBuilder()
-        builder.addCommands(TeamCommand(), TagCommand(), ChooseCommand())
+        builder.addCommands(
+            TeamCommand(),
+            TagCommand(),
+            AboutCommand(Color.GREEN, "https://github.com/riptakagi/mk8dx-teambot", Permission.VIEW_CHANNEL, Permission.MESSAGE_WRITE, Permission.MESSAGE_READ),
+            GuildlistCommand(eventWaiter),
+            ChooseCommand()
+        )
 
         builder.setOwnerId("695218967173922866")
         builder.setPrefix("_")
@@ -53,7 +62,7 @@ class Listener : ListenerAdapter() {
         timer.schedule(object : TimerTask() {
             override fun run() {
                 event.jda.apply {
-                    presence.setPresence(OnlineStatus.ONLINE, Activity.watching("github.com/notsmatch/mk8dx-teambot | ${guilds.size} servers"))
+                    presence.setPresence(OnlineStatus.ONLINE, Activity.watching("${Bot.instance.WEBSITE} | ${guilds.size} servers"))
                 }
             }
         }, 0, 1000*300)
