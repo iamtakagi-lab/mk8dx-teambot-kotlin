a92003ed97a76bf72b98c00f4049d008f489d5c3
notsmatch <smatchjp@gmail.com>
2020-07-14 06:29:31 +0900

    feat: kana tag command

---
+++ src/main/kotlin/me/notsmatch/teambot/command/KanaTagCommand.kt
@@ -0,0 +1,57 @@
+package me.notsmatch.teambot.command
+
+import com.jagrosh.jdautilities.command.Command
+import com.jagrosh.jdautilities.command.CommandEvent
+import me.notsmatch.teambot.util.TagUtils
+import me.notsmatch.teambot.util.NumberUtils
+import net.dv8tion.jda.api.EmbedBuilder
+import org.apache.commons.lang3.StringUtils
+import java.awt.Color
+
+class KanaTagCommand : Command() {
+
+    init {
+        this.name = "kanatag"
+        this.help = "ランダムなひらがなでタグを決めます"
+        this.arguments = "<文字数>"
+    }
+
+    override fun execute(event: CommandEvent?) {
+        event?.apply {
+            val args = StringUtils.split(args)
+
+            var tagLength: Int = 0
+            if (args.isNotEmpty()) {
+
+                if (!NumberUtils.isInteger(args[0])) {
+                    return reply(EmbedBuilder().apply {
+                        setColor(Color.RED)
+                        setAuthor(
+                            "Error",
+                            null,
+                            null
+                        )
+                        setDescription("文字数は1~10で指定してください\n``_tag <文字数>``")
+                    }.build())
+                }
+
+                tagLength = args[0].toInt()
+
+                if (tagLength > 10 || tagLength < 1) {
+                    return reply(EmbedBuilder().apply {
+                        setColor(Color.RED)
+                        setAuthor(
+                            "Error",
+                            null,
+                            null
+                        )
+                        setDescription("文字数は1~10で指定してください\n``_tag <文字数>``")
+                    }.build())
+                }
+
+                //結果出力
+                reply("タグ: " + TagUtils.randomHiragana(tagLength))
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/me/notsmatch/teambot/util/TagUtils.kt
@@ -0,0 +1,66 @@
+package me.notsmatch.teambot.util
+
+import me.notsmatch.teambot.Bot
+import java.lang.StringBuilder
+
+object TagUtils {
+
+    var alphabet: CharArray = CharArray(26)
+
+    val hiragana = arrayOf(
+        "あ", "い", "う", "え", "お",
+        "か", "き", "く", "け", "こ",
+        "さ", "し", "す", "せ", "そ",
+        "た", "ち", "つ", "て", "と",
+        "な", "に", "ぬ", "ね", "の",
+        "は", "ひ", "ふ", "へ", "ほ",
+        "ま", "み", "む", "め", "も",
+        "や", "ゆ", "よ",
+        "ら", "り", "る", "れ", "ろ",
+        "わ", "を", "ん"
+    )
+
+    val num = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+    init {
+        var c = 'a'
+        for (i in 0..25) {
+            alphabet[i] = c++
+        }
+    }
+
+    fun getAlphabet(i: Int) : String {
+        return alphabet[i].toString()
+    }
+
+    fun randomAlphabets(length: Int): String{
+        val tag = StringBuilder()
+
+        for(i in 0 until length){
+            tag.append(getAlphabet(Bot.random.nextInt(alphabet.size)))
+        }
+
+        return tag.toString() + " | " + tag.toString().toUpperCase()
+    }
+
+    fun randomHiragana(length: Int): String{
+        val tag = StringBuilder()
+
+        for(i in 0 until length){
+            tag.append(hiragana[Bot.random.nextInt(hiragana.size)])
+        }
+
+        return tag.toString()
+    }
+
+    fun randomNum(length: Int): String{
+        val tag = StringBuilder()
+
+        for(i in 0 until length){
+            tag.append(num[Bot.random.nextInt(num.size)])
+        }
+
+        return tag.toString()
+    }
+
+}
